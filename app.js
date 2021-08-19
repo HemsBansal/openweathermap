d042cbf693cb27fcef4ab022787e93e04b2f30a5
Hemant Kumar <hemsbansal@gmail.com>
2021-08-19 16:23:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ button.addEventListener('click', function(name){
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=50a7aa80fa492fa92e874d23ad061374')
 .then(response => response.json())
 .then(data => {
-  var tempValue = data['main']['temp']-273;
+  var tempValue = Math.floor(data['main']['temp']-273);
   var nameValue = data['name'];
   var descValue = data['weather'][0]['description'];
 
